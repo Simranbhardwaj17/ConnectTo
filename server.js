0649e4348974d249f59290f92d811d8da64bf1b7
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-01-21 10:33:20 +0530

    Basic Express Setup

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');  //bring express just to get a very simple express server up and running
 
-const app = express();  //initialize our app var with express
+const app = express();  //initialize app var with express
 
-app.get('/', (req, res) => res.send('API Running'));
-
+app.get('/', (req, res) => res.send('API Running'));  //create a single endpoint to test
+           //res.send:-send data to browser
+           
 const PORT = process.env.PORT || 5000   //put the port in a var              (it will look for an environment var called PORT to use & when deployed to heroku,tht's wher it's gonna get the port no, locally run  on port 5000)
      // So basically, if ther's no env var set, it will go def to 5000
 
