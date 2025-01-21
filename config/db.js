407c4bc59e380bc7a1ab37c348819004fa9d7398
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-01-21 18:47:56 +0530

    Connecting To MongoDB With Mongoose

---
+++ config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');  // bring mongoose, to connect
+
+//bring conifig pkg, to grab sthat str that is inside default.json
+const config = require('config');
+
+//to get URI put that in a var cld db, do config.get & we can get any of the values in that json file
+const db = config.get('mongoURI');
+
+//create async arrow fn, we need somethig to call within our server.js
+//use try catch, if DB unable to connect & show error msg
+//when use async/await, wrap it inside try catch block
+//Since mongoose.connect() returns promise, put await before that
+const connectDB = async () => {
+    try{
+        await mongoose.connect(db);  //pass db as param & leave other for now
+        console.log('MongoDB Connected...');      
+    } catch(err) {
+        console.log(err.message);  //pass err value which has a msg prop in it
+        //Exit process with failure
+        process.exit(1)  //to esc from the process with failure
+    }
+}
+
+module.exports = connectDB  //export the mthd
