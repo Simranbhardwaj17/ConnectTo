8ffe8c419e44b8b34c19b72eb3d9ce0ea1f57be7
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-01-22 10:51:44 +0530

    Define routes in server file

--- server.js
+++ server.js
@@ -8,16 +8,19 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));  //create a single endpoint to test
            //res.send:-send data to browser
-           
-const PORT = process.env.PORT || 5000   //put the port in a var              (it will look for an environment var called PORT to use & when deployed to heroku,tht's wher it's gonna get the port no, locally run  on port 5000)
+    
+
+//define Routes(for all routes file)
+app.use('/api/users', require('./routes/api/users'));     //put endpt(/)  &   api/users pertain to user's file  &  api/users:-pertain to ('/') of users file
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+
+const PORT = process.env.PORT || 5000   //put the port in a var  (it will look for an environment var called PORT to use & when deployed ,tht's where it's gonna get the port no, locally run on port 5000)
      // So basically, if ther's no env var set, it will go def to 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`)) //take that var & listen on a port  ( in it pass the port & then do a callbk, print, when connection happen)
-
-
-
-
-
 
 
 //It is the main entry file
