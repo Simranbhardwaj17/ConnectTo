1ce475ef80e47d48e723f89d897de30349875cea
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-01-24 23:54:02 +0530

    Retrieving users by their unique identifiers

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,24 @@
 const express = require('express');  // bring exp
 const router = express.Router(); //to use exp router, create a var called router & set to express.Router();
+const auth = require('../../middleware/auth')            //bring middleware, outside of api. routes into middleware then auth
 
+const User = require('../../models/User');
 //create route(don't do app.get/post.)
 //We do router.get('/', callbk) req        for callbk use arr fn with req res
 // @route     GET api/auth
 // @desc      Test route(reg users,..)
 // @access    Public                     @access value whether pub or pvt
-router.get('/', (req, res) => res.send('Auth route'));  //its just a test route
+//to use middleware, add as 2nd parameter
+router.get('/', auth, async (req, res) => {
+    // do a try-catch to make a call to our database using async/await
+    try {            
+        const user = await User.findById(req.user.id).select('-password');             //.select('-password'):- leave off the passw in the data(not ret passw)
+        res.json(user);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+});  //only adding middleware(auth) make this route protected.
 
 //export router
-module.exports = router;  
+module.exports = router; 
