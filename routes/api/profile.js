e617f6b60f7634ea77192dfe16a46b03ff6a5f67
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-01-26 12:44:58 +0530

    Create & Update Profile Routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,11 @@
 const express = require('express');  
 const router = express.Router(); 
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+// bring in normalize to give us a proper url, regardless of what user entered
+const normalize = require('normalize-url');
+// import normalizeUrl from 'normalize-url';
+
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
@@ -26,6 +31,77 @@ router.get('/me', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });  
+
+// we're gonna create the route to actually create or update a profile. So this is probably gonna be our longest route, like the most code within a single route.
+
+
+// @route     POST api/profile          api/profile is for all pfp
+// @desc      Create or Update user profile
+// @access    Private 
+// URL or the endpoint is slash only coz of /pfp only
+// we need to use the auth middleware, but we also need to use the validation middleware, so what we have to do in that case is put in a set of brackets like this, and put the first middleware which is auth, and then the second one is the brackets with the checks.
+router.post('/', [
+    auth, [
+        check('status', 'Status is required').not().isEmpty(),
+        check('skills', 'Skills is required').not().isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const {
+            website,
+            skills,
+            youtube,
+            twitter,
+            instagram,
+            linkedin,
+            facebook,
+            // spread the rest of the fields we don't need to check
+            ...rest
+        } = req.body;
+
+        //Build profile object
+        const profileFields = {
+            user: req.user.id,
+            website:
+              website && website !== ''
+                ? normalize(website, { forceHttps: true })
+                : '',
+            skills: Array.isArray(skills)
+              ? skills
+              : skills.split(',').map((skill) => ' ' + skill.trim()),
+            ...rest
+          };
+      
+        // Build socialFields object
+        const socialFields = { youtube, twitter, instagram, linkedin, facebook };
+      
+        // normalize social fields to ensure valid url
+        for (const [key, value] of Object.entries(socialFields)) {
+            if (value && value.length > 0)
+                socialFields[key] = normalize(value, { forceHttps: true });
+        }
+        // add to profileFields
+        profileFields.social = socialFields;
+      
+        try {
+            // Using upsert option (creates new doc if no match is found):
+            let profile = await Profile.findOneAndUpdate(
+                { user: req.user.id },
+                { $set: profileFields },
+                { new: true, upsert: true, setDefaultsOnInsert: true }
+            );
+            return res.json(profile);
+        } catch (err) {
+            console.error(err.message);
+            return res.status(500).send('Server Error');
+        }
+    }
+);
 
 //export router
 module.exports = router;  
