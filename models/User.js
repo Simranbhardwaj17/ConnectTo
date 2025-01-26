2abf3fcc9d1a1bd739e3e4b3d5defc057910eabb
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-01-26 23:27:54 +0530

    Fix modelname(user) in mongoose.model()

--- models/User.js
+++ models/User.js
@@ -1,34 +1,31 @@
 //Naming convention for models, use uppercase at beginning
 
-const mongoose = require('mongoose');  //bring mongoose
+const mongoose = require('mongoose');    //bring mongoose
 
 //create schema(take obj with all fields)
-const UserSchema = new mongoose.Schema({ 
-    name: {
-        type: String,
-        required:  true
-    },
-    email: {          //login with email &passw rather than having a separate field for username
-        type: String,
-        required:  true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required:  true
-    },
-    avatar: {
-        type: String
-    },
-    date: {
-        type: Date, 
-        default: Date.now
-    }
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    //login with email & passw rather than having a separate field for username
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports = User = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('user', UserSchema);
 // mongoose.model('modelName', Schema)
-
-//Gravatar: allows u to attach a pfp img to ur email(I want to be this in user model so that its always accessible coz at 1st reg, it create user not pfp, have to go through process to create pfp)
-
-//Avatar available right away. So, its here
