d1f25adc332cc6a3644da7839c572438232db2b1
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-01-28 20:58:35 +0530

    Add Post Route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,43 @@
-const express = require('express');  // bring exp
+const express = require('express'); // bring exp
 const router = express.Router(); //to use exp router, create a var called router & set to express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route     GET api/posts
-// @desc      Test route
-// @access    Public                     @access value whether pub or pvt
-router.get('/', (req, res) => res.send('Posts route'));  //its just a test route
+const auth = require('../../middleware/auth');
+const Post = require('../../models/Post');
+const User = require('../../models/User');
+
+// @route    POST api/posts
+// @desc     Create a post
+// @access   Private
+router.post(
+  '/',
+  auth,
+  check('text', 'Text is required').notEmpty(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 //export router
-module.exports = router;  
+module.exports = router
