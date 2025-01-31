33274d6170a718db4ea83bdf8a1d1ad074ecfc60
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-01-31 23:13:16 +0530

    Import store

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,25 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+// Redux
+import { Provider } from 'react-redux';  //Provider connects the two, because Redux is a separate, it's separate from React but this will combine them together. And we can do that by surrounding our entire app inside with this provider
+import store from './store';
+
 import './App.css';
 
 const App = () => ( 
-  <Router>
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </div>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  </Provider>
 );
 
 export default App;
