a72246d3958bfe9a771a7a0cac75b2d99f9a4de3
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-02-01 20:15:03 +0530

    createStore is deprecated

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,5 @@
-import { createStore, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { thunk } from 'redux-thunk';           // (Named Import)     //thunk is a middleware
 import rootReducer from './reducers';
@@ -9,8 +10,8 @@ const initialState = {};         //All of our initial state will be in the reduc
 
 const middleware = [thunk];  //create a variable called middleware and set that to an array. And the only middleware we have is Thunk.
 
-const store = createStore(      //create a variable called store set that to createStore, which we brought in from Redux. And this is gonna take in, first of all, the root reducer. and then the initialState, and then any middleware.
-  rootReducer,
+const store = configureStore({      //create a variable called store set that to createStore, which we brought in from Redux. And this is gonna take in, first of all, the root reducer. and then the initialState, and then any middleware.
+  reducer: rootReducer},
   initialState,
   composeWithDevTools(applyMiddleware(...middleware))  //since we're using the DevTools extension, we can use composeWithDevTools And then that takes in the Apply middleware and inside Apply Middleware we just use the Spread Operator and add that middleware variable that we created.
 );
